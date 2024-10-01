b17caa446bed5b6c0f79df5f63c2b887cb2a4c67
yura <yricog1290@gmail.com>
2024-10-01 22:18:47 +0900

    tailwind config추가, font 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,33 +7,44 @@ export default {
     screens: {
       tablet: '640px',
       // => @media (min-width: 640px) { ... }
-
       laptop: '1024px',
       // => @media (min-width: 1024px) { ... }
-
       desktop: '1280px',
       // => @media (min-width: 1280px) { ... }
     },
     colors: {
-      primary: '#ff7849',
-      secondary: '#ffc82c',
+      'pm-500': '#FF5D05',
+      'pm-700': '#B54204',
+      'sc-700': '#8A9910',
+      'sc-800': '#6F7B0D',
+      'gr-800': '#3A3D40',
+      'gr-700': '#505458',
+      'gr-600': '#8E9398',
+      'gr-500': '#B5B9BD',
+      'gr-50': '#F3F4F5',
       white: '#ffffff',
-      'gray-dark': '#273444',
-      gray: '#8492a6',
-      'gray-light': '#d3dce6',
+      black: '#000000',
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    container: {
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-      },
+      pretendard: ['Pretendard', 'sans-serif'],
     },
     extend: {
+      fontSize: {
+        12: '0.75rem',
+        14: '0.875rem',
+        16: '1rem',
+        18: '1.125rem',
+        20: '1.25rem',
+        22: '1.375rem',
+        24: '1.5rem',
+        26: '1.625rem',
+        28: '1.75rem',
+      },
+      fontWeight: {
+        regular: '400',
+        semi: '600',
+        bold: '800',
+      },
       spacing: {
         13: '3.25rem', // p-13, m-13, h-13
         15: '3.75rem',
@@ -41,7 +52,10 @@ export default {
         144: '36rem',
       },
       borderRadius: {
-        '4xl': '2rem',
+        'br-4': '0.25rem',
+        'br-6': '0.375rem',
+        'br-12': '0.75rem',
+        'br-16': '1rem',
       },
     },
   },
